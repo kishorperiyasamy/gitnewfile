6d965e811d28c4f1ccf52dbfb71cfe21fd4e1ac9
kishorperiyasamy <periyasamykishor@gmail.com>
2022-06-12 19:42:18 +0530

    some statement added

---
+++ src/org/gitsample/sample.java
@@ -0,0 +1,21 @@
+package org.gitsample;
+
+public class sample {
+
+		public static void main(String[] args) {
+			System.out.println(2);
+			System.out.println(3);
+			System.out.println(4);
+			System.out.println(5);
+			System.out.println(6);
+			System.out.println(7);
+			
+			
+			
+			
+			
+		}
+
+	
+
+}
